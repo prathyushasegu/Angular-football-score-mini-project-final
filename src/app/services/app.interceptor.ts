292c8b4c94343216dbb7198bad5ea0c577e8a21a
prathyushasegu <prathyusha1252@gmail.com>
2023-09-19 03:55:28 +0530

    1 file modified

--- src/app/services/app.interceptor.ts
+++ src/app/services/app.interceptor.ts
@@ -16,7 +16,7 @@ export class TokenInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // Add your API key to the request headers here
-    const apiKey = '4ba557971ddc0a16ce6aa02dde020ac8';
+    const apiKey = '6703a81d79d95d81e2dbb55eca2b5281';
     const modifiedRequest = request.clone({
       setHeaders: {
         'x-rapidapi-key': `${apiKey}`,
